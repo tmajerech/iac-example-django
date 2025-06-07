c2e1802978cd8a55c77d337f14bf7910ef23d5a1
tmajerech <majerech.t@gmail.com>
2025-06-07 16:44:59 +0200

    updated ecr upload script

--- scripts/bootstrap-push-initial.sh
+++ scripts/bootstrap-push-initial.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
 # Usage:
-#   ./bootstrap-push-initial.sh [env_name] [tag] [profile]
+#   ./bootstrap-push-initial.sh [repo_name] [profile] [tag]
 # Defaults:
-#
 #   profile = default
 #   tag = initial
 
-ENV_NAME=${1:-prod}
-TAG=${2:-latest}
-PROFILE=${3:-default}
-
-REPO="$ENV_NAME-django_images"
+REPO_NAME=${1}
+PROFILE=${2:-default}
+TAG=${3:-initial}
 AWS_REGION="eu-central-1"
 
 # Get AWS account ID using specified profile
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text --profile "$PROFILE")
 
 # Full ECR repo URI
-ECR_URI="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO"
+ECR_URI="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME"
 
-echo "Building Docker image for $REPO:$TAG"
-docker build --platform linux/amd64 -t $REPO .
+echo "Building Docker image for $REPO_NAME:$TAG"
+docker build --platform linux/amd64 -t $REPO_NAME .
 
 echo "Tagging image as $ECR_URI:$TAG"
-docker tag $REPO $ECR_URI:$TAG
+docker tag $REPO_NAME $ECR_URI:$TAG
 
 echo "Logging in to ECR with profile $PROFILE..."
 aws ecr get-login-password --region $AWS_REGION --profile "$PROFILE" \
